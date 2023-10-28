bc4bf3a687cf91597ea649f3150c7f1523ae9398
hotehama2022 <hotehama-r@cin-group.com>
2023-10-28 10:52:10 +0900

    会員登録画面の実装

---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import { Box, Container, Typography } from '@mui/material';
+import { RegistForm } from '@/components/registForm';
+
+export default function Register() {
+  return (
+    <>
+      <Head>
+        <title>つぶやきくん会員登録</title>
+        <meta name="description" content="つぶやきくん会員登録" />
+      </Head>
+      <Container maxWidth="xs">
+        <Box marginTop={5}>
+          <Typography variant="h5" textAlign={'center'} marginBottom={3}>
+            つぶやきくん会員登録
+          </Typography>
+          <RegistForm />
+        </Box>
+      </Container>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import { Header } from '@/layouts/header';
 
 export default function Home() {
   return (
@@ -7,10 +6,7 @@ export default function Home() {
       <Head>
         <title>つぶやきくん</title>
         <meta name="description" content="つぶやきくん" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/logo.png" />
       </Head>
-      <Header />
     </>
   );
 }
